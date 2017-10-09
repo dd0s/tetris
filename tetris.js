b5fa43c80bb5bbf96527c3c94055fe8f76b50616
dd0s <pvvner@yandex.ru>
2017-10-09 03:13:09 +0300

    playerRotate()

--- tetris.js
+++ tetris.js
@@ -84,12 +84,22 @@ function playerMove(dir) {
 }
 
 function playerRotate(dir) {
+    let offset = 1;
     rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
 }
 
 function rotate(matrix, dir) {
     for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < matrix[y].length; ++x) {
+        for (let x = 0; x < y; ++x) {
             [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
         }
     }
